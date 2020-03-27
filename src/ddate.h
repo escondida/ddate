8b1d6ed319e12c6c4f48833ddeaaa424b3bf9556
Ivy Foster <escondida@iff.ink>
2020-03-27 13:55:26 -0500

    document sloganeer(), since malloc is involved

--- src/ddate.h
+++ src/ddate.h
@@ -1,4 +1,11 @@
 #pragma once
 
+/* sloganeer(void):
+	The perfect exhortatory mechanism:
+	brooks no arguments but returns a mallocked string
+	containing a delightful* catchphrase.
+
+	* Catchphrase not actually guaranteed to be delightful.
+*/
 char *
 sloganeer(void);
