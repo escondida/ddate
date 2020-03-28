efc1eb98117823d1f201e4783ddad9b7b3f1ced7
Ivy Foster <escondida@iff.ink>
2020-03-27 20:09:52 -0500

    Add functions to handle Saint Tib's Day

---
+++ src/tibs.c
@@ -0,0 +1,48 @@
+/* tibs.c
+
+	Functions for dealing with that most wonderful of all days,
+	St. Tib's Day, called "Leap Day" by the uninitiated.
+*/
+
+#include <stdbool.h>
+#include <stdint.h>
+
+#include "tibs.h"
+
+bool
+is_tibsyear(int32_t year);
+
+bool
+is_past_tibsday(int32_t y, int32_t d)
+{
+	if (is_tibsyear(y) && d > TIBSDAY) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+bool
+is_tibsday(int32_t y, int32_t d)
+{
+	if (is_tibsyear(y) && d == TIBSDAY) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+bool
+is_tibsyear(int32_t y)
+{
+	/* https://en.wikipedia.org/wiki/Leap_year#Algorithm */
+	if (y % 4 != 0) {
+		return false;
+	} else if (y % 100 != 0) {
+		return true;
+	} else if (y % 400 != 0) {
+		return false;
+	} else {
+		return true;
+	}
+}
---
+++ src/tibs.h
@@ -0,0 +1,16 @@
+/* tibs.h
+
+	Functions for dealing with that most wonderful of all days,
+	St. Tib's Day, called "Leap Day" by the uninitiated.
+*/
+
+#pragma once
+
+/* 59: 31 + 29 - 1 (to account for fencepost error) */
+#define TIBSDAY 59
+
+bool
+is_past_tibsday(int32_t year, int32_t day);
+
+bool
+is_tibsday(int32_t year, int32_t day);
