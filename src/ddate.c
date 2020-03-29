c2e17ccf6406ff6deb8d5b29a29001a6816412e2
Ivy Foster <escondida@iff.ink>
2020-03-28 19:42:49 -0500

    Remove unused functions, tweak main() to use new ones

--- src/ddate.c
+++ src/ddate.c
@@ -53,379 +53,98 @@
 
 #include "ddate.h"
 
-#include "slogans.h"
-
-/* configuration options  VVVVV   READ THIS!!! */
-
-/* If you wish ddate(1) to print the date in the same format as Druel's
- * original ddate when called in immediate mode, define OLD_IMMEDIATE_FMT
- */
-
-#define OLD_IMMEDIATE_FMT
-
-/* If you wish to use the US format for aneristic dates (m-d-y), as opposed to
- * the Commonwealth format, define US_FORMAT.
- */
-
-/* #define US_FORMAT */
-
-/* work around hacks for standalone package */
-#define PACKAGE_STRING "standalone"
-
-#ifndef __GNUC__
-	#define inline /* foo */
-#endif
-
-#ifndef KILL_BOB
-int
-xday_countdown(int yday, int year);
-#endif
-
-struct disc_time {
-	int season; /* 0-4 */
-	int day; /* 0-72 */
-	int yday; /* 0-365 */
-	int year; /* 3066- */
-};
-
-char default_fmt[] = "%{%A, %B %d%}, %Y YOLD";
-char *default_immediate_fmt =
-#ifdef OLD_IMMEDIATE_FMT
-		"Today is %{%A, the %e day of %B%} in the YOLD %Y%N%nCelebrate %H"
-#else
-		default_fmt
-#endif
-		;
-
-#define DY(y) (y + 1166)
-
-static inline char *
-ending(int i)
-{
-	return i / 10 == 1
-			? "th"
-			: (i % 10 == 1
-					   ? "st"
-					   : (i % 10 == 2 ? "nd" : (i % 10 == 3 ? "rd" : "th")));
-}
-
-static inline int
-leapp(int i)
-{
-	return (!(DY(i) % 4)) && ((DY(i) % 100) || (!(DY(i) % 400)));
-}
+#include "convert.h"
+#include "fmt.h"
 
 void
-format(char *buf, const char *fmt, struct disc_time dt);
-
-struct disc_time
-convert(int, int);
-struct disc_time
-makeday(int, int, int);
+usage(FILE *f, char *programname);
 
 int
 main(int argc, char *argv[])
 {
 	time_t t;
-	struct tm *eris;
-	int bob, raw;
-	struct disc_time hastur;
-	char schwa[23 * 17], *fnord = 0;
-	int pi;
-	char *progname, *p;
+	struct tm *greg;
+	struct ddate dd = {0};
+	char *fnord = 0;
+	int32_t optind;
+	char *todayfmt = "Today is %{%W, the %d%o day of %S %} in the YOLD %Y%N%n%tCelebrate %H!";
+	char *otherfmt = "%{%W, %S %d%}, YOLD %Y%N (%H)";
+	char *progname = argv[0], *p;
 
+	/* Do this here instead of in slogans.c in case we add more randomness */
 	srandom((uint32_t)time(NULL));
 
-	progname = argv[0];
 	if ((p = strrchr(progname, '/')) != NULL) {
 		progname = p + 1;
 	}
 
 	/* do args here */
-	for (pi = 1; pi < argc; pi++) {
-		switch (argv[pi][0]) {
+	for (optind = 1; optind < argc; optind++) {
+		switch (argv[optind][0]) {
 		case '+':
-			fnord = argv[pi] + 1;
+			fnord = argv[optind] + 1;
 			break;
 		case '-':
-			switch (argv[pi][1]) {
-			case 'V':
-				printf(("%s (%s)\n"), progname, PACKAGE_STRING);
+			switch (argv[optind][1]) {
+			case 'h':
+				usage(stdout, argv[0]);
+				return EXIT_SUCCESS;
+			case 'v':
+				printf(("%s\n"), progname);
+				return EXIT_SUCCESS;
 			default:
-				goto usage;
+				usage(stderr, argv[0]);
+				return EXIT_FAILURE;
 			}
+		/*
+			Break the loop on the first non-format,
+			non-option argument. Yes, unfortunately that
+			means that the order of arguments really does
+			matter.
+		*/
 		default:
 			goto thud;
 		}
 	}
 
 thud:
-	if (argc - pi == 3) {
-		int moe = atoi(argv[pi]), larry = atoi(argv[pi + 1]),
-			curly = atoi(argv[pi + 2]);
-		hastur = makeday(
-#ifdef US_FORMAT
-				moe, larry,
-#else
-				larry, moe,
-#endif
-				curly);
-		if (hastur.season == -1) {
-			printf("Invalid date -- out of range\n");
-			return -1;
-		}
-		fnord = fnord ? fnord : default_fmt;
-	} else if (argc != pi) {
-	usage:
-		fprintf(stderr, ("usage: %s [+format] [day month year]\n"), argv[0]);
-		exit(1);
-	} else {
-		t = time(NULL);
-		eris = localtime(&t);
-		bob = eris->tm_yday; /* days since Jan 1. */
-		raw = eris->tm_year; /* years since 1980 */
-		hastur = convert(bob, raw);
-		fnord = fnord ? fnord : default_immediate_fmt;
-	}
-	format(schwa, fnord, hastur);
-	printf("%s\n", schwa);
+	/* Y[Y[Y[Y]]] M[M] D[D] */
+	if (argc - optind == 3) {
+		int32_t moe = atoi(argv[optind]);
+		int32_t larry = atoi(argv[optind + 1]);
+		int32_t curly = atoi(argv[optind + 2]);
 
-	return 0;
+		if (!ddate_greg_to_eris2(&dd, moe, larry, curly)) {
+			fputs("Invalid date\n", stderr);
+			return EXIT_FAILURE;
+		}
+
+		fnord = fnord ? fnord : otherfmt;
+	} else if (argc == optind) {
+		t = time(NULL);
+		greg = localtime(&t);
+		fnord = fnord ? fnord : todayfmt;
+		/* tm_year starts counting at 1900 */
+		if (!ddate_greg_to_eris(&dd, greg->tm_year + 1900, greg->tm_yday)) {
+			fputs("Invalid date\n", stderr);
+			return EXIT_FAILURE;
+		}
+	} else {
+		usage(stderr, argv[0]);
+		return EXIT_FAILURE;
+	}
+
+	char buf[1024];
+	if (!ddate_fmt(buf, sizeof(buf), dd, fnord)) {
+		return EXIT_FAILURE;
+	}
+
+	puts(buf);
+	return EXIT_SUCCESS;
 }
 
 void
-format(char *buf, const char *fmt, struct disc_time dt)
+usage(FILE *f, char *prog)
 {
-	int tib_start = -1, tib_end = 0;
-	int i, fmtlen = strlen(fmt);
-	char *bufptr = buf;
-
-	char *day_long[5] = {"Sweetmorn", "Boomtime", "Pungenday",
-						 "Prickle-Prickle", "Setting Orange"};
-
-	char *day_short[5] = {"SM", "BT", "PD", "PP", "SO"};
-
-	char *season_long[5] = {"Chaos", "Discord", "Confusion", "Bureaucracy",
-							"The Aftermath"};
-
-	char *season_short[5] = {"Chs", "Dsc", "Cfn", "Bcy", "Afm"};
-
-	char *holyday[5][2] = {{"Mungday", "Chaoflux"},
-						   {"Mojoday", "Discoflux"},
-						   {"Syaday", "Confuflux"},
-						   {"Zaraday", "Bureflux"},
-						   {"Maladay", "Afflux"}};
-
-	/*    fprintf(stderr, "format(%p, \"%s\", dt)\n", buf, fmt);*/
-
-	/* first, find extents of St. Tib's Day area, if defined */
-	for (i = 0; i < fmtlen; i++) {
-		if (fmt[i] == '%') {
-			switch (fmt[i + 1]) {
-			case 'A':
-			case 'a':
-			case 'd':
-			case 'e':
-				if (tib_start >= 0) {
-					tib_end = i + 1;
-				} else {
-					tib_start = i;
-					tib_end = i + 1;
-				}
-				break;
-			case '{':
-				tib_start = i;
-				break;
-			case '}':
-				if (tib_start >= 0) {
-					tib_end = i + 1;
-					break;
-				} else {
-					fputs("St. Tib's day can't end until after it starts!",
-						  stderr);
-					goto eschaton;
-				}
-			}
-		}
-	}
-
-	/* now do the formatting */
-	buf[0] = 0;
-
-	for (i = 0; i < fmtlen; i++) {
-		if ((i == tib_start) && (dt.day == -1)) {
-			/* handle St. Tib's Day */
-			strcpy(bufptr, ("St. Tib's Day"));
-			bufptr += strlen(bufptr);
-			i = tib_end;
-		} else {
-			if (fmt[i] == '%') {
-				bool sloganeered = false;
-				char *wibble = 0, snarf[23];
-
-				switch (fmt[++i]) {
-				case 'A':
-					wibble = day_long[dt.yday % 5];
-					break;
-				case 'a':
-					wibble = day_short[dt.yday % 5];
-					break;
-				case 'B':
-					wibble = season_long[dt.season];
-					break;
-				case 'b':
-					wibble = season_short[dt.season];
-					break;
-				case 'd':
-					sprintf(snarf, "%d", dt.day + 1);
-					wibble = snarf;
-					break;
-				case 'e':
-					sprintf(snarf, "%d%s", dt.day + 1, ending(dt.day + 1));
-					wibble = snarf;
-					break;
-				case 'H':
-					if (dt.day == 4 || dt.day == 49) {
-						wibble = holyday[dt.season][dt.day == 49];
-					}
-					break;
-				case 'N':
-					if (dt.day != 4 && dt.day != 49) {
-						goto eschaton;
-					}
-					break;
-				case 'n':
-					*(bufptr++) = '\n';
-					break;
-				case 't':
-					*(bufptr++) = '\t';
-					break;
-
-				case 'Y':
-					sprintf(snarf, "%d", dt.year);
-					wibble = snarf;
-					break;
-				case '.':
-					sloganeered = true;
-					wibble = sloganeer();
-					break;
-#ifndef KILL_BOB
-				case 'X':
-					sprintf(snarf, "%d", xday_countdown(dt.yday, dt.year));
-					wibble = snarf;
-					break;
-#endif /* KILL_BOB */
-				}
-
-				if (wibble) {
-					/*		    fprintf(stderr, "wibble = (%s)\n", wibble);*/
-					strcpy(bufptr, wibble);
-					bufptr += strlen(wibble);
-				}
-
-				if (sloganeered) {
-					free(wibble);
-				}
-			} else {
-				*(bufptr++) = fmt[i];
-			}
-		}
-	}
-eschaton:
-	*(bufptr) = 0;
+	fprintf(f, "Usage: %s [+format] [year month day]\n", prog);
 }
-
-struct disc_time
-makeday(int imonth, int iday, int iyear) /*i for input */
-{
-	struct disc_time funkychickens;
-
-	int cal[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-	int dayspast = 0;
-
-	memset(&funkychickens, 0, sizeof(funkychickens));
-	/* basic range checks */
-	if (imonth < 1 || imonth > 12 || iyear == 0) {
-		funkychickens.season = -1;
-		return funkychickens;
-	}
-	if (iday < 1 || iday > cal[imonth - 1]) {
-		if (!(imonth == 2 && iday == 29 && iyear % 4 == 0 &&
-			  (iyear % 100 != 0 || iyear % 400 == 0))) {
-			funkychickens.season = -1;
-			return funkychickens;
-		}
-	}
-
-	imonth--;
-	/*  note: gregorian year 0 doesn't exist so
-	 *  add one if user specifies a year less than 0 */
-	funkychickens.year = iyear + 1166 + ((0 > iyear) ? 1 : 0);
-	while (imonth > 0) {
-		dayspast += cal[--imonth];
-	}
-	funkychickens.day = dayspast + iday - 1;
-	funkychickens.season = 0;
-	if ((funkychickens.year % 4) == 2) {
-		if (funkychickens.day == 59 && iday == 29) {
-			funkychickens.day = -1;
-		}
-	}
-	funkychickens.yday = funkychickens.day;
-	/*               note: EQUAL SIGN...hopefully that fixes it */
-	while (funkychickens.day >= 73) {
-		funkychickens.season++;
-		funkychickens.day -= 73;
-	}
-	return funkychickens;
-}
-
-struct disc_time
-convert(int nday, int nyear)
-{
-	struct disc_time funkychickens;
-
-	funkychickens.year = nyear + 3066;
-	funkychickens.day = nday;
-	funkychickens.season = 0;
-	if ((funkychickens.year % 4) == 2) {
-		if (funkychickens.day == 59) {
-			funkychickens.day = -1;
-		} else if (funkychickens.day > 59) {
-			funkychickens.day -= 1;
-		}
-	}
-	funkychickens.yday = funkychickens.day;
-	while (funkychickens.day >= 73) {
-		funkychickens.season++;
-		funkychickens.day -= 73;
-	}
-	return funkychickens;
-}
-
-#ifndef KILL_BOB
-
-/* Code for counting down to X-Day, X-Day being Cfn 40, 3164
- *
- * After `X-Day' passed without incident, the CoSG declared that it had
- * got the year upside down --- X-Day is actually in 8661 AD rather than
- * 1998 AD.
- *
- * Thus, the True X-Day is Cfn 40, 9827.
- *
- */
-
-int
-xday_countdown(int yday, int year)
-{
-	int r = (185 - yday) + (((yday < 59) && (leapp(year))) ? 1 : 0);
-	while (year < 9827) {
-		r += (leapp(++year) ? 366 : 365);
-	}
-	while (year > 9827) {
-		r -= (leapp(year--) ? 366 : 365);
-	}
-	return r;
-}
-
-#endif
